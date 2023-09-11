cda8992c80a13680540324040acca794b6fcbc16
wonjunJang <nuriyooyoung@gmail.com>
2023-09-11 13:05:30 +0900

    modify

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ def send_message():
 def univ_ratio_check():
     current_time = time.strftime("%H:%M:00")
     print(current_time)
-    univ_ratio("대학경쟁률 실시간 조회")
+    univ_ratio("대학경쟁률 실시간 조회(학교별로 업데이트 시간이 달라요!)")
     
     
 
--- modules/sw.py
+++ modules/sw.py
@@ -6,16 +6,31 @@ class univ_ratio:
     def __init__(self,time):
         self.time = time
         gacheon_sw = 'https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10190361.html'
+        gatalic_sw = 'https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10030221.html'
+        
         result = urlopen(gacheon_sw)
+        result2 = urlopen(gatalic_sw)
+        
         html = result.read()
+        html2 = result2.read()
+        
         soup = BeautifulSoup(html, 'html.parser')
+        soup2 = BeautifulSoup(html2, 'html.parser')
+        
         temp = soup.find("table", {'class':'tableRatio2'})
+        temp2 = soup2.find("table", {'class':'tableRatio3'})
+        
         data = []
+        data2 = []
+        
         for td in soup.find_all('td'):
             data.append(td.text.strip())
-
+        for td in soup2.find_all('td'):
+            data2.append(td.text.strip())
+            
         print(time)
-        result = "전형 이름 : " + str(data[27]), "| 모집인원수 "+ data[28] +"명" + " | 실시간 지원자수 "+ data[29]+"명"," | 실시간 경쟁률 " + data[30]
+        print("가천대 AI/SW 전형 : " + str(data[1079]), "| 모집인원 "+ data[1080] +"명" + " | 실시간 지원자수 "+ data[1081]+"명"," | 실시간 경쟁률 " + data[1082])
+        print("가톨릭대 잠재능력우수자(면접전형) : " + str(data2[492]), "| 모집인원 "+ data2[493] +"명" + " | 실시간 지원자수 "+ data2[494]+"명"," | 실시간 경쟁률 " + data2[495])
         print("scuccessfully")
         print(result)
         
