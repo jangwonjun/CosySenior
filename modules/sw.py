c0985cdcc7a2d991805a226135572f3a5620dd75
wonjunJang <nuriyooyoung@gmail.com>
2023-09-13 12:51:47 +0900

    modify

--- modules/sw.py
+++ modules/sw.py
@@ -27,6 +27,7 @@ class univ_ratio:
         kwang_sw = 'http://ratio.uwayapply.com/Sl5KTjlKZiUmOiZKemZUZg=='
         soongsil_sw = 'https://addon.jinhakapply.com/RatioV1/RatioH/Ratio11010301.html'
         hangyang_sw = 'https://addon.jinhakapply.com/RatioV1/RatioH/Ratio11640281.html'
+        donguck_sw = 'https://addon.jinhakapply.com/RatioV1/RatioH/Ratio10550291.html'
             
         result = urlopen(gacheon_sw)
         result2 = urlopen(gatalic_sw)
@@ -37,6 +38,7 @@ class univ_ratio:
         result7 = urlopen(kwang_sw)
         result8 = urlopen(soongsil_sw)
         result9 = urlopen(hangyang_sw)
+        result10 = urlopen(donguck_sw)
         
         html = result.read()
         html2 = result2.read()
@@ -47,6 +49,7 @@ class univ_ratio:
         html7 = result7.read()
         html8 = result8.read()
         html9 = result9.read()
+        html10 = result10.read()
         
         soup = BeautifulSoup(html, 'html.parser')
         soup2 = BeautifulSoup(html2, 'html.parser')
@@ -57,6 +60,7 @@ class univ_ratio:
         soup7 = BeautifulSoup(html7, 'html.parser')
         soup8 = BeautifulSoup(html8, 'html.parser')
         soup9 = BeautifulSoup(html9, 'html.parser')
+        soup10 = BeautifulSoup(html10, 'html.parser')
         
         temp = soup.find("table", {'class':'tableRatio2'})
         temp2 = soup2.find("table", {'class':'tableRatio3'})
@@ -67,7 +71,9 @@ class univ_ratio:
         temp7 = soup7.find_all("tr", {'class':'trFieldValue'})
         temp8 = soup8.find_all("table", {'class':'tableRatio3'})
         temp9 = soup9.find_all("table", {'class':'tableRatio3'})
+        temp10 = soup10.find_all("table",{'class':'tableRatio3'})
         
+        data10 = []
         data9 = []
         data8 = []
         data7 = []
@@ -96,10 +102,10 @@ class univ_ratio:
             data8.append(td.text.strip())
         for td in soup9.find_all('td'):
             data9.append(td.text.strip())
-       
+        for td in soup10.find_all('td'):
+            data10.append(td.text.strip())
   
         
-         
         gacheon = "가천대 AI/SW 전형 : " + str(data[1079]), "| 모집인원 "+ data[1080] +"명" + " | 실시간 지원자수 "+ data[1081]+"명"," | 실시간 경쟁률 " + data[1082]
         gatalic =  "가톨릭대 잠재능력우수자(면접전형) : " + str(data2[492]), "| 모집인원 "+ data2[493] +"명" + " | 실시간 지원자수 "+ data2[494]+"명"," | 실시간 경쟁률 " + data2[495]
         gagede = "서울과기대 인공지능응용학과 : " + str(data3[284]), "| 모집인원 "+ data3[285] +"명" + " | 실시간 지원자수 "+ data3[286]+"명"," | 실시간 경쟁률 " + data3[287]
@@ -109,14 +115,14 @@ class univ_ratio:
         kwang = "광운대 특기자전형  : " + data7[280] +  " | 모집인원 "+ data7[281] +"명" + " | 실시간 지원자수 "+ data7[282]+"명"," | 실시간 경쟁률 " + data7[283]
         soongsil = "숭실대 특기자전형  : " + data8[728] +  " | 모집인원 "+ data8[729] +"명" + " | 실시간 지원자수 "+ data8[730]+"명"," | 실시간 경쟁률 " + data8[731]
         hangyang = "한양대 특기자전형  : " + data9[850] +  " | 모집인원 "+ data9[851] +"명" + " | 실시간 지원자수 "+ data9[852]+"명"," | 실시간 경쟁률 " + data9[853]
+        donguck = "동국대 doream  : " + data10[286] +  " | 모집인원 "+ data10[287] +"명" + " | 실시간 지원자수 "+ data10[288]+"명"," | 실시간 경쟁률 " + data10[289]
         
         print(server_message)
-        #print(gist)
         
         final_result2 = []
         send_final_result = []
-        final_result = "\n".join([str(gacheon), str(gatalic), str(gagede), str(unist), str(dgist), str(kwang), str(soongsil), str(hangyang)])
-        check_univ_ratio = [str(data[1082]),"가천대"],[str(data2[495]),"가톨릭대"], [str(data3[287]),"서울과기대"],[str(data4[15]),"GIST"],[str(data5[9]),"UNIST"],[str(data6[15]),"DGIST"],[str(data7[283]),"광운대"],[str(data8[731]),"숭실대"],[str(data9[853]),"한양대"]  
+        final_result = "\n".join([str(gacheon), str(gatalic), str(gagede), str(unist), str(dgist), str(kwang), str(soongsil), str(hangyang),str(donguck)])
+        check_univ_ratio = [str(data[1082]),"가천대"],[str(data2[495]),"가톨릭대"], [str(data3[287]),"서울과기대"],[str(data4[15]),"GIST"],[str(data5[9]),"UNIST"],[str(data6[15]),"DGIST"],[str(data7[283]),"광운대"],[str(data8[731]),"숭실대"],[str(data9[853]),"한양대"],[str(data10[289]),"동국대"]
         check_univ_ratio = sorted(check_univ_ratio, key=lambda x: x[0],reverse=True)
         for i in range(len(check_univ_ratio)):
             final_result2.append(check_univ_ratio[i][1])
